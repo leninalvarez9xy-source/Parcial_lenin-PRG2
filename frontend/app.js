d3b33c88c8c6f77e51821afabfcec6c1f34c7917
lenin <leninalvarez9xy@gmail.com>
2026-05-28 10:34:14 -0500

    nuevo cambio

--- frontend/app.js
+++ frontend/app.js
@@ -1,18 +1,30 @@
 // Atrapamos el formulario cuando el usuario le da a "Ingresar"
 document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evita que la página parpadee o se recargue
 
-    // Obtenemos lo que el usuario escribió
-    const usuario = document.getElementById('usuario').value;
-    const password = document.getElementById('password').value;
-    const mensaje = document.getElementById('mensaje');
+```
+e.preventDefault();
 
-    // Validación de prueba
-    if(usuario === "admin" && password === "1234") {
-        window.location.href = "exito.html";
-        // Aquí luego pondremos el código para redirigir a la página del modelo predictivo
-    } else {
-        mensaje.style.color = "red";
-        mensaje.textContent = "Usuario o contraseña incorrectos.";
-    }
+// Datos ingresados
+const usuario = document.getElementById('usuario').value;
+const password = document.getElementById('contraseña').value;
+const mensaje = document.getElementById('mensaje');
+
+// Validación
+if(usuario === "admin" && password === "1234") {
+
+    mensaje.style.color = "green";
+    mensaje.textContent = "Acceso correcto";
+
+    setTimeout(() => {
+        window.location.href = "/exito";
+    }, 1000);
+
+} else {
+
+    mensaje.style.color = "red";
+    mensaje.textContent = "Datos incorrectos";
+
+}
+```
+
 });
